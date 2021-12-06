29028cca7e8da238bad306e25cb6ad077a40f7df
exer <35124914+NightSploit@users.noreply.github.com>
2021-12-06 17:13:51 +0800

    Update omni_H8296.mk

--- omni_H8296.mk
+++ omni_H8296.mk
@@ -26,7 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/sony/H8296/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 #$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
